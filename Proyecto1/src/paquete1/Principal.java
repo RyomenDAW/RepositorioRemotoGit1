d38faa7c8d5d49674f76ecd46c887ae6407cabf1
alumnado <alumnado@A120-PC09>
2024-02-20 14:43:38 +0100

    primer commit 1

---
+++ Proyecto1/src/paquete1/Principal.java
@@ -0,0 +1,13 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ninja 125 H2 sin silenciador");
+		System.out.println("broom broom broommmmmm");
+
+	}
+
+}
+
