e2de262dffc331e1eae6f9350cc95863faed5fab
alumnado <alumnado@A120-PC09>
2024-03-05 13:38:54 +0100

    He añadido un comentario sobre angry birds 5 March 2024 13:38 PUSH --> COMMIT

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,8 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ninja 125 H2 sin silenciador");
-		System.out.println("broom broom broommmmmm");
-
+		System.out.println("broom broom broommmmmm,,,,");
+		//Comentario angry bird shiny
 	}
 
 }
